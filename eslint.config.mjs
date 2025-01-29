28e094795acd5a9e530cad7f65c398d283717e1e
yoann <yoann.ochietti@gmail.com>
2025-01-29 15:59:24 -0500

    Revert eslint config to ESM

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,7 +2,7 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 
 /** @type {import('eslint').Linter.Config[]} */
-module.exports = [
+export default [
   { languageOptions: { globals: { ...globals.nodeBuiltin, ...globals.jest } } },
   pluginJs.configs.recommended,
 ];
